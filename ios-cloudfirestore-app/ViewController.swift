ee5ff289fd1a9f2e7028e22a6d3fe237bc23dc83
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-11-06 23:25:22 +0900

    Initial commit

---
+++ ios-cloudfirestore-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-cloudfirestore-app
+//
+//  Created by Shuhei Kagaya on 2018/11/06.
+//  Copyright © 2018年 Shuhei Kagaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
