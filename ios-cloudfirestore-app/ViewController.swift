54585fcac19eec02087d32841dca239c0f1bcc71
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-11-13 23:15:56 +0900

    追加ボタン仮実装

--- ios-cloudfirestore-app/ViewController.swift
+++ ios-cloudfirestore-app/ViewController.swift
@@ -4,14 +4,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var content: UITextField!
+    
+    @IBAction func onClickAdd(_ sender: UIButton) {
+        let db = Firestore.firestore()
+        db.collection("todos").addDocument(data: [
+            "content": content.text,
+            "updatedAt": FieldValue.serverTimestamp(),
+            "createdAt": FieldValue.serverTimestamp(),
+            ]) { err in
+                if let err = err {
+                    
+                } else {
+                    
+                }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
